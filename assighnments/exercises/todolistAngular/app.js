5662ec054a779fd5b7fe7b9ebf961875d91b74ee
tabarak <mosawit313@gmail.com>
2017-05-11 14:58:38 +0300

    still need edit and know how to do post and akked it's need stylinnnng

--- assighnments/exercises/todolistAngular/app.js
+++ assighnments/exercises/todolistAngular/app.js
@@ -1,6 +1,7 @@
 var app = angular.module("myApp", []);
 app.controller("mainCtrl", function ($scope, $http) {
 
+
     $http.get("https://api.vschool.io/tabarak/todo").then(function (response) {
 
         console.log(response.data)
@@ -11,13 +12,21 @@ app.controller("mainCtrl", function ($scope, $http) {
             title: $scope.title,
             imgUrl: $scope.imgUrl,
             price: $scope.price,
-            description: $scope.description
+            description: $scope.description,
+            checkBox: $scope.checkBox
         }
         $http.post('https://api.vschool.io/tabarak/todo/', $scope.todoList).then(function (obj) {
             $scope.arr.push(obj)
         }, function (err) {
             console.log(err.status)
         });
-
+    }
+    $scope.remove = function (item) {
+        $http.delete("https://api.vschool.io/tabarak/todo/" + item._id).then(function (response) {
+            $http.get("https://api.vschool.io/tabarak/todo").then(function (response) {
+                console.log(response.data)
+                $scope.arr = response.data
+            })
+        })
     }
 })
